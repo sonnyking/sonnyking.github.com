8e722d545b38dfb874e25c64f9aacd0b3a259407
Sonny King <sonny@sonnyking.com>
2015-08-11 23:44:50 -0700

    adding typescript

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ var gulp = require('gulp');
 // Include Our Plugins
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -21,14 +22,17 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('dist/css'));
 });
 
-// Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+  var tsResult = gulp.src('src/**/*.ts')
+    .pipe(ts({
+        declarationFiles: true,
+        noExternalResolve: true
+      }));
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest('dist/definitions')),
+    tsResult.js.pipe(gulp.dest('dist/js'))
+    ]);
 });
 
 // Watch Files For Changes
